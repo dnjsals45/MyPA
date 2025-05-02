ba6aeb6d5cd058258aebc2f6840257d49e775c1c
dnjsals45 <dnjsals45@naver.com>
2025-05-02 20:36:45 +0900

    build: jjwt 라이브러리 등록

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,6 +34,12 @@ dependencies {
 
     // swagger
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.6")
+
+    // jjwt
+    implementation("io.jsonwebtoken:jjwt-api:0.12.6")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
+
 }
 
 kotlin {
